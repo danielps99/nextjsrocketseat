f13f247ff728d95c5f5615d2af5869ce0a86c03c
Daniel <danielps99@gmail.com>
2021-02-27 16:19:09 -0300

    Barra ExperienceBar com html e css somente

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>Hello word</h1>
+    <div className="container">
+      <ExperienceBar />
+      <h1>Hello word</h1>
+    </div>
   );
 }
 
