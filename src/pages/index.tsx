effe332ff0e425c8d96355b1c098d1d8fd074775
Daniel <danielps99@gmail.com>
2021-02-28 16:57:38 -0300

    Criando componente ChallengeBox. Agora aprenderemos Contextos no React

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css';
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -19,7 +22,7 @@ export default function Home() {
         <Countdown />
       </div>
       <div>
-
+        <ChallengeBox />
       </div>
     </section>
   </div>
