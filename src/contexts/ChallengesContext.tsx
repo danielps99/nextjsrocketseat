7d323f22f7d2bc7e531155441bd8fef2fb65da55
Daniel <danielps99@gmail.com>
2021-02-28 22:30:41 -0300

    Notificacao sonora e visual para o usuario

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 
 import challenges from '../../challenges.json';
 
@@ -7,6 +7,7 @@ interface Challenge {
     description: string
     amount: number
 }
+
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
@@ -32,6 +33,10 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
     const [activeChallenge, setActiveChallenge] = useState(null);
     const experienceToNextLeval = Math.pow((level + 1) * 4, 2);
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
+
     function levelUp() {
         setLevel(level + 1);
     }
@@ -40,6 +45,15 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+        
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio', {
+                body:`Valendo ${challenge.amount} xp!`
+            })
+            
+        }
     }
 
     function resetChallenge(){
